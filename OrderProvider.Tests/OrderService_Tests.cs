86b6f0ba4096de03baa80cf7651a994a05b56ff0
Dyssling <olle.lundgarden@hotmail.com>
2024-11-06 14:31:23 +0100

    CreateOrderAsync test split up into two parts. One for an admin, and one for a guest user.

--- OrderProvider.Tests/OrderService_Tests.cs
+++ OrderProvider.Tests/OrderService_Tests.cs
@@ -13,11 +13,11 @@ public class OrderService_Tests
 
     [Fact]
 
-    public void CreateOrderAsync_ShouldCreateAnOrder_And_ReturnTrue()
+    public void CreateOrderAsync_ShouldCreateAnOrderAsAnAdmin_And_ReturnTrue()
     {
         // Arrange
 
-        //An addressModel and productModel is created, for use in the orderModel
+        //An addressModel, userModel and productModel is created, for use in the orderModel
 
         var addressModel = new AddressModel()
         {
@@ -31,7 +31,7 @@ public class OrderService_Tests
             Country = "Sweden"
         };
 
-		var userModel = new UserModel()
+		var userModel = new UserModel() //An admin user is created
 		{
 			Id = new Guid().ToString(),
 			Address = addressModel,
@@ -71,5 +71,66 @@ public class OrderService_Tests
 
         Assert.True(result); //It should return true
     }
-	
+
+    [Fact]
+
+    public void CreateOrderAsync_ShouldCreateAnOrderAsGuest_And_ReturnTrue()
+    {
+        // Arrange
+
+        //An addressModel and productModel is created, for use in the orderModel
+
+        var addressModel = new AddressModel()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Street = "gata",
+            City = "Kalmar",
+            State = "Depression",
+            PhoneNumber = "123790",
+            ZipCode = "39350",
+            CountryCallingCode = "+46",
+            Country = "Sweden"
+        };
+
+        var userModel = new UserModel() //A guest user is created
+        {
+            Id = new Guid().ToString(),
+            Address = addressModel,
+            Role = "Guest"
+        };
+
+        var productModel = new ProductModel()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Stövel",
+            Description = "Bootstrap Bill",
+            Stock = 20,
+            Price = 100m
+        };
+
+        var productList = new List<ProductModel>(); //A productList is created, which will be inserted into the orderModel
+
+        productList.Add(productModel); //The previously created productModel is added to the productList
+
+        var orderModel = new OrderModel
+        {
+            Id = Guid.NewGuid().ToString(),
+            User = userModel,
+            ShippingChoice = "PostNord Standard",
+            ProductList = productList,
+            PriceTotal = 100m,
+            IsConfirmed = true,
+        };
+
+        _orderService.Setup(x => x.CreateOrderAsync(orderModel)).Returns(true); //CreateOrderAsync in the mocked orderService is setup to return true when an orderModel is inserted
+
+        // Act
+
+        var result = _orderService.Object.CreateOrderAsync(orderModel); //CreateOrderAsync is called, with the created orderModel
+
+        // Assert
+
+        Assert.True(result); //It should return true
+    }
+
 }
