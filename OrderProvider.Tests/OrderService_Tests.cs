278936639e589035b9e5325c596f9a747ee71306
Dyssling <olle.lundgarden@hotmail.com>
2024-11-06 13:29:52 +0100

    OrderService tests begun.

--- OrderProvider.Tests/OrderService_Tests.cs
+++ OrderProvider.Tests/OrderService_Tests.cs
@@ -1,5 +1,9 @@
-﻿using Infrastructure.Interfaces;
+﻿using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Infrastructure.Repositories;
 using Moq;
+using Newtonsoft.Json;
 
 namespace OrderProvider.Tests;
 
@@ -7,5 +11,11 @@ public class OrderService_Tests
 {
     private readonly Mock<IOrderService> _orderService = new Mock<IOrderService>();
 
+    [Fact]
+
+    public async Task CreateOrderAsync_ShouldCreateAnOrderEntityInDatabase_And_ReturnTrue()
+    {
+
+    }
 
 }
