858dc0938b4b3cd3b54cad7713e2b8d3300cb089
Dyssling <olle.lundgarden@hotmail.com>
2024-11-08 10:58:27 +0100

    GetAddressAsync method in IOrderService modified to GetUserAddressAsync. Tests completed for this method.

--- Infrastructure/Interfaces/IOrderService.cs
+++ Infrastructure/Interfaces/IOrderService.cs
@@ -10,7 +10,7 @@ public interface IOrderService
     IEnumerable<OrderModel> GetAllUserOrdersAsync();
 	bool UpdateOrderAsync(OrderModel model);
 	bool DeleteOrderAsync(OrderModel model);
-	AddressModel GetAddressAsync(string id);
+	AddressModel GetUserAddressAsync(string userId);
 	IEnumerable<ProductModel> GetProductListAsync(string id);
     IEnumerable<ProductModel> DeleteProductFromList(List<ProductModel> productList, string productId, int amount); //amount = The amount of instances of the product you want to delete (in case of multiple instances of the same product in the list)
     CartModel GetUserCartAsync(string userId);
--- OrderProvider.Tests/OrderService_Tests.cs
+++ OrderProvider.Tests/OrderService_Tests.cs
@@ -1145,4 +1145,41 @@ public class OrderService_Tests
 		Assert.True(firstUserOrders.First().CreationDate == thirdOrder.CreationDate);
 		Assert.True(firstUserOrders.Last().CreationDate == firstOrder.CreationDate);
 	}
+
+    [Fact]
+    public void GetUserAddressAsync_ShouldReturnCorrectAddress()
+    {
+        // Arrange
+        var addressModel = new AddressModel() //An address object is created
+        {
+            Id = Guid.NewGuid().ToString(),
+            Street = "gata",
+            City = "Kalmar",
+            State = "Depression",
+            PhoneNumber = "123790",
+            ZipCode = "39350",
+            CountryCallingCode = "+46",
+            Country = "Sweden"
+        };
+
+        var userModel = new UserModel() //A registered user is created
+        {
+            Id = new Guid().ToString(),
+            Address = addressModel,
+            Role = "User"
+        };
+
+
+        _orderService.Setup(x => x.GetUserAddressAsync(userModel.Id)).Returns(addressModel); //GetUserAddressAsync is setup to return the simulated address object, if the corresponding userId is inserted
+
+        // Act
+
+        var result = _orderService.Object.GetUserAddressAsync(userModel.Id); //GetUserAddressAsync method is called
+
+        // Assert
+
+        Assert.NotNull(result); //The returned value should not be null
+        Assert.IsType<AddressModel>(result); //The returned object should be of the type AddressModel
+        Assert.Equal(addressModel.Id, result.Id); //The Id of the returned address object should be equal to the Id of the address object created above
+    }
 }
